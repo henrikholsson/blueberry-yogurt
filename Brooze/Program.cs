f6768b1248ddac75fa4cefd8a3784ca45678c9be
Henrik Olsson <h.h.olsson@gmail.com>
2021-08-01 16:20:29 +0200

    setup packages

---
+++ Brooze/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Brooze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Silverstoned/Silverstoned/CheeseDoodle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Silverstoned
+{
+    public class CheeseDoodle
+    {
+        private readonly string _name;
+        private readonly string _seed;
+        public CheeseDoodle(string name, string seed)
+        {
+            _name = name;
+            _seed = seed;
+        }
+
+        private string Cypher()
+        {
+            string cypherText = "";
+            int characterCount = 0;
+            foreach(char nameCharacter in _name)
+            {   
+                
+                foreach(char seedCharacter in _seed)
+                {
+                    cypherText = cypherText + seedCharacter;
+                }
+                characterCount++;
+            }
+            return cypherText;
+        }
+
+        public string DeCypher(string name)
+        {
+
+        }
+    }
+}
